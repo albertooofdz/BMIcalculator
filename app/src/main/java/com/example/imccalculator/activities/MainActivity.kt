0cdf59e37c5a248903666c0956d3a71fbaa31ca4
albertooofdz <119025836+albertooofdz@users.noreply.github.com>
2024-02-08 14:01:50 +0100

    Create first inteface of IMC calculator

---
+++ app/src/main/java/com/example/imccalculator/activities/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.imccalculator.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imccalculator.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+    fun initView(){
+
+        var initialWeight = 65
+
+
+
+
+
+
+    }
+
+
+}
+
