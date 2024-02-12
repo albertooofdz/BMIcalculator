491169b55e9e6c17b23a1efa60842f6392744045
albertooofdz <119025836+albertooofdz@users.noreply.github.com>
2024-02-12 14:03:04 +0100

    Added functions

--- app/src/main/java/com/example/imccalculator/activities/MainActivity.kt
+++ app/src/main/java/com/example/imccalculator/activities/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.imccalculator.activities
-
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.graphics.drawable.toDrawable
 import com.example.imccalculator.R
+import com.example.imccalculator.R.drawable
 import com.google.android.material.slider.RangeSlider
 import kotlinx.coroutines.awaitAll
+import java.text.DecimalFormat
+import kotlin.math.pow
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +26,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var heightText: TextView
     var initialHeight = 160
     var initialWeight = 60
+    var initialIMC: Float = 1f
+    lateinit var resultText : TextView
+    lateinit var calculateButton:Button
+    lateinit var age : EditText
+    lateinit var maleSign : AppCompatButton
+    lateinit var femaleSign : AppCompatButton
+
 
 
 
@@ -37,6 +52,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         fun initView() {
 
             btnAddWeight= findViewById(R.id.PlusBtn)
@@ -44,6 +60,12 @@ class MainActivity : AppCompatActivity() {
             weightText= findViewById(R.id.weightText)
             heightSlider=findViewById(R.id.heightRangeSlider)
             heightText=findViewById(R.id.heightText)
+            resultText=findViewById(R.id.resultText)
+            calculateButton=findViewById(R.id.calculateButton)
+            age=findViewById(R.id.ageEditText)
+            maleSign=findViewById(R.id.maleButton)
+            femaleSign=findViewById(R.id.femaleButton)
+
 
             btnAddWeight.setOnClickListener{
 
@@ -66,16 +88,26 @@ class MainActivity : AppCompatActivity() {
                 heightText.text="${getString(R.string.Altura)} ${initialHeight.toString()} cm"
 
             }
+            calculateButton.setOnClickListener {
+                val decimalFormat = DecimalFormat("#.##")
+                initialIMC = initialWeight / (initialHeight.toFloat() / 100).pow(2)
+                resultText.text = "${decimalFormat.format(initialIMC)}"
+            }
 
+            maleSign.setOnClickListener { 
+                femaleSign.background = femaleSign.(R.drawable.female_sign_unselected)
 
-
-
-
-
+                maleSign.background =
+                
+                
+            }
+            
 
         }
 
 
 
     }
+
+
 
